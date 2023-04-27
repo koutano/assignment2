28174e943f14162d93d02cc110ef047cd299d53f
koutano <koutano0311@gmail.com>
2023-04-27 12:16:56 +0900

    third commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,44 @@ namespace assignment2
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            Syoberu syoberu = new Syoberu();
+
+            car.Stop();
+            syoberu.Stop();
+            car.Run();
+            syoberu.Run();
+            syoberu.Bring();
+
+            Console.WriteLine(syoberu.status);
+        }
+    }
+
+    class Car
+    {
+        public int status { get; protected set; }
+
+        public void Run()
+        {
+            status = 0;
+        }
+
+        public void Stop()
+        {
+            status = 1;
+        }
+    }
+
+    class Syoberu : Car
+    {
+        public void Dig()
+        {
+            status = 2;
+        }
+
+        public void Bring()
+        {
+            status = 3;
         }
     }
 }
